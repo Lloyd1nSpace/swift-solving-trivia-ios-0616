9e6d4ac50b495ac0fa6d98c1f9bfce8b5761c690
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-12 23:36:14 -0400

    Fianlly

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -11,12 +11,32 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var statesAndCapitals: [String : String] = [:]
     
-    /*
-     
-     write your function here
-     
-     */
-
+    func solveTrivia(triviaDictionary: [String : String]) -> String {
+        
+        var stateWithNoSimilarCharacters: String = ""
+        
+        statesAndCapitals = triviaDictionary
+        
+        for (state, capital) in statesAndCapitals {
+            
+            let stateLetters = Array(state.lowercaseString.characters)
+            let cityLetters = Array(capital.lowercaseString.characters)
+            
+            var sharedLetter = false
+            
+            for letter in cityLetters {
+                if letter != " " && stateLetters.contains(letter) {
+                    sharedLetter = true
+                    break
+                }
+            }
+            if sharedLetter == false {
+                stateWithNoSimilarCharacters = state
+            }
+        }
+        print("\n\n\nThe only state whose capital contains none of the characters as the state name is: \(stateWithNoSimilarCharacters)\n\n\n")
+        return stateWithNoSimilarCharacters
+    }
 }
-
